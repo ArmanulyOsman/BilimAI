abba8cd048cc0ed7bb40d30c4ce9ea77d4022c08
oarmanuly <oarmanuly@jmart.kz>
2026-04-12 13:32:12 +0500

    fix css

--- BilimAI/settings.py
+++ BilimAI/settings.py
@@ -80,3 +80,5 @@ LOGOUT_REDIRECT_URL = '/login/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
